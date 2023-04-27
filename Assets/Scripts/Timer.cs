59064e590a1773fe30d746f1a5707733f3ea8cb6
adcimon <37084114+adcimon@users.noreply.github.com>
2023-04-27 16:39:23 +0200

    Implemented scene systems

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using TMPro;
+
+[RequireComponent(typeof(TMP_Text))]
+public class Timer : MonoBehaviour
+{
+    private TMP_Text text;
+    private float time;
+
+    private void Awake()
+    {
+        text = GetComponent<TMP_Text>();
+    }
+
+    private void Update()
+    {
+        time += Time.deltaTime;
+
+        int hours = Mathf.FloorToInt(time / 3600f);
+        int minutes = Mathf.FloorToInt(time / 60f);
+        int seconds = Mathf.FloorToInt(time % 60f);
+        int milliseconds = Mathf.FloorToInt((time * 100f) % 100f);
+        text.text = hours.ToString("00") + ":" + minutes.ToString("00") + ":" + seconds.ToString("00") + ":" + milliseconds.ToString("00");
+    }
+}
